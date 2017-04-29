5912b553d5345d4ab7bd9184a1cb895387f45751
Vince P <ianparulan@gmail.com>
2017-04-29 11:28:14 -0400

    added config

---
+++ config.js
@@ -0,0 +1,26 @@
+// User-specific configuration
+exports.conversationWorkspaceId = ''; // replace with the workspace identifier of your conversation
+
+// Create the credentials object for export
+exports.credentials = {};
+
+// Watson Conversation
+// https://www.ibm.com/watson/developercloud/conversation.html
+exports.credentials.conversation = {
+	password: '926a1087-efee-4aba-8c6d-a066e0d36f68',
+	username: 'bHzJQDqeaOQb'
+};
+
+// Watson Speech to Text
+// https://www.ibm.com/watson/developercloud/speech-to-text.html
+exports.credentials.speech_to_text = {
+	password: '5ac75cd0-c399-4950-b360-93ae0d39dbc0',
+	username: '4JZHW8TKmY0q'
+};
+
+// Watson Text to Speech
+// https://www.ibm.com/watson/developercloud/text-to-speech.html
+exports.credentials.text_to_speech = {
+	password: 'f18f7690-2689-4b95-817a-b9c49752f558',
+	username: 'JrHay1ujiYwt'
+};
